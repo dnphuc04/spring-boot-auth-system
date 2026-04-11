fadea252f44e9cbe876473b1992a68d63cf9b287
dnphuc1211 <phuckdhd123@gmail.com>
2026-04-11 22:27:02 +0700

    create UserRepository for database abstraction and add UserService for business logic handling

---
+++ src/main/java/com/example/auth_system/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.example.auth_system.controller;
+
+import com.example.auth_system.entity.User;
+import com.example.auth_system.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users") // Tất cả API trong class này đều bắt đầu bằng /users
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    // API 1: Tạo người dùng mới (Nhận dữ liệu JSON từ Postman)
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    // API 2: Lấy danh sách tất cả người dùng
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+}
